6f706231509a38918be5b7f84ee927c10e35bf40
Riabov.AA <riabov.aa@parmalogica.ru>
2025-12-08 15:29:20 +0500

    Найти число, у которого нет пары

---
+++ LeetCode/Tests/HashTable/PracticalApplication/ContainsDuplicateTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests.HashTable.PracticalApplication;
+
+/// <summary>
+/// Contains Duplicate https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1112/
+/// </summary>
+public class ContainsDuplicateTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 1 }, true)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, false)]
+    [InlineData(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+    public void Test(int[] arr, bool expected)
+    {
+        var result = ContainsDuplicate(arr);
+        Assert.Equal(expected, result);
+    }
+
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!hashSet.Add(nums[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ LeetCode/Tests/HashTable/PracticalApplication/SingleNumberTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Tests.HashTable.PracticalApplication;
+
+/// <summary>
+/// Найти число, у которого нет пары https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1176/
+/// </summary>
+public class SingleNumberTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 2, 1 }, 1)]
+    [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
+    [InlineData(new int[] { 1 }, 1)]
+    public void Test1(int[] nums, int expected)
+    {
+        var result = SingleNumber(nums);
+        Assert.Equal(expected, result);
+    }
+
+    // идея в том, что одинаковые числи при XOR дают ноль. Надо числа представить в двоичном виде и сделать XOR
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach (int num in nums)
+        {
+            result ^= num; // XOR (исключающее или, 1 только когда числа различаются)
+        }
+        return result;
+    }
+
+    public int SingleNumber1(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+        foreach (var t in nums)
+        {
+            if (!hashSet.Add(t))
+            {
+                hashSet.Remove(t);
+            }
+        }
+        return hashSet.First();
+    }
+}
