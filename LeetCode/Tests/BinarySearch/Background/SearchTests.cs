36fef5b01c128835c507e8297de026912de780df
aariabov <aariabov@mail.ru>
2026-03-07 09:50:00 +0500

    [Корень числа](https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/)

---
+++ LeetCode/Tests/BinarySearch/Background/SearchTests.cs
@@ -0,0 +1,78 @@
+﻿namespace Tests.BinarySearch.Background;
+
+/// <summary>
+/// [Бинарный поиск в массиве](https://leetcode.com/explore/learn/card/binary-search/138/background/1038/)
+/// </summary>
+public class SearchTests
+{
+    [Theory]
+    [InlineData(new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
+    [InlineData(new int[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
+    [InlineData(new int[] { 2, 5 }, 0, -1)]
+    public void Test1(int[] nums, int target, int expected)
+    {
+        var result = Search(nums, target);
+        Assert.Equal(expected, result);
+    }
+
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            // Вычисляем средний индекс так, чтобы избежать переполнения целого числа
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid; // Элемент найден
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1; // Цель в правой половине
+            }
+            else
+            {
+                right = mid - 1; // Цель в левой половине
+            }
+        }
+
+        return -1; // Элемент не найден
+    }
+
+    // работает и быстро, рекурсия
+    public int MySearch(int[] nums, int target)
+    {
+        return Rec(0, nums.Length - 1);
+
+        int Rec(int startIdx, int endIdx)
+        {
+            if (
+                startIdx < 0
+                || startIdx > nums.Length - 1
+                || endIdx < 0
+                || endIdx > nums.Length - 1
+            )
+            {
+                return -1;
+            }
+            if (startIdx == endIdx)
+            {
+                return nums[startIdx] == target ? startIdx : -1;
+            }
+
+            var midIdx = ((endIdx - startIdx) / 2) + startIdx;
+            if (nums[midIdx] == target)
+            {
+                return midIdx;
+            }
+            else if (target < nums[midIdx])
+            {
+                return Rec(startIdx, midIdx - 1);
+            }
+            return Rec(midIdx + 1, endIdx);
+        }
+    }
+}
---
+++ LeetCode/Tests/BinarySearch/Template1/MySqrtTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Tests.BinarySearch.Template1;
+
+/// <summary>
+/// [Корень числа](https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/)
+/// </summary>
+public class MySqrtTests
+{
+    [Theory]
+    [InlineData(4, 2)]
+    [InlineData(8, 2)]
+    [InlineData(2147395600, 46340)]
+    [InlineData(6, 2)]
+    [InlineData(36, 6)]
+    [InlineData(1, 1)]
+    [InlineData(2, 1)]
+    public void Test1(int x, int expected)
+    {
+        var result = MySqrt(x);
+        Assert.Equal(expected, result);
+    }
+
+    public int MySqrt(int x)
+    {
+        if (x < 2)
+        {
+            return x;
+        }
+
+        var left = 2;
+        var right = x / 2;
+        var result = 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left + 1) / 2;
+            if (mid <= x / mid)
+            {
+                left = mid + 1;
+                result = mid;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public int MyBitMySqrt(int x)
+    {
+        var i = 1;
+        while (i < 46341 && i * i < x)
+        {
+            i = i << 1;
+        }
+        i = i >> 1;
+        while (i * i < x)
+        {
+            i++;
+        }
+
+        return i * i == x ? i : i - 1;
+    }
+
+    // решение в лоб, падает по таймауту
+    public int MyMySqrt(int x)
+    {
+        var i = 1;
+        var cur = 1;
+        while (cur < x)
+        {
+            i++;
+            cur = i * i;
+        }
+
+        return cur == x ? i : i - 1;
+    }
+}
