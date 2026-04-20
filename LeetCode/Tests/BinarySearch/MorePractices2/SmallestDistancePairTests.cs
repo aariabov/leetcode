3ecb3955aaae2d9ea3aabe54f921c55dc63d9c84
aariabov <aariabov@mail.ru>
2026-04-20 15:18:44 +0500

    [Разбить массив на подмассивы с минимальной суммой](https://leetcode.com/explore/learn/card/binary-search/146/more-practices-ii/1042/)

---
+++ LeetCode/Tests/BinarySearch/MorePractices2/SmallestDistancePairTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Tests.BinarySearch.MorePractices2;
+
+// [Наименьшее расстояние между элементами](https://leetcode.com/explore/learn/card/binary-search/146/more-practices-ii/1041/)
+public class SmallestDistancePairTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 1 }, 1, 0)]
+    [InlineData(new[] { 1, 1, 1 }, 2, 0)]
+    [InlineData(new[] { 1, 6, 1 }, 3, 5)]
+    public void Test(int[] nums, int k, int expected)
+    {
+        var res = SmallestDistancePair(nums, k);
+        Assert.Equal(expected, res);
+    }
+
+    public int SmallestDistancePair(int[] nums, int k)
+    {
+        // 1. Сортировка массива для работы метода двух указателей
+        Array.Sort(nums);
+
+        int n = nums.Length;
+        // 2. Устанавливаем границы бинарного поиска
+        // Минимальная возможная дистанция — 0, максимальная — разница между краями массива
+        int left = 0;
+        int right = nums[n - 1] - nums[0];
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            // 3. Считаем количество пар, дистанция которых <= mid
+            if (CountPairs(nums, mid) < k)
+            {
+                // Если пар слишком мало, искомая дистанция больше mid
+                left = mid + 1;
+            }
+            else
+            {
+                // Иначе искомая дистанция может быть mid или меньше
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private int CountPairs(int[] nums, int mid)
+    {
+        int count = 0;
+        int left = 0;
+
+        // Используем два указателя (скользящее окно)
+        for (int right = 0; right < nums.Length; right++)
+        {
+            // Пока разница между элементами больше допустимой (mid),
+            // сдвигаем левую границу
+            while (nums[right] - nums[left] > mid)
+            {
+                left++;
+            }
+            // Все пары между left и right включительно (с правым концом в right)
+            // имеют дистанцию <= mid
+            count += right - left;
+        }
+
+        return count;
+    }
+
+    // работает, но не проходит по скорости
+    public int MySmallestDistancePair(int[] nums, int k)
+    {
+        var res = new List<int>();
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                res.Add(Math.Abs(nums[i] - nums[j]));
+            }
+        }
+
+        res.Sort();
+        return res[k - 1];
+    }
+}
---
+++ LeetCode/Tests/BinarySearch/MorePractices2/SplitArrayTests.cs
@@ -0,0 +1,119 @@
+﻿namespace Tests.BinarySearch.MorePractices2;
+
+// [Разбить массив на подмассивы с минимальной суммой](https://leetcode.com/explore/learn/card/binary-search/146/more-practices-ii/1042/)
+public class SplitArrayTests
+{
+    [Theory]
+    [InlineData(new[] { 7, 2, 5, 10, 8 }, 2, 18)]
+    [InlineData(new[] { 1, 2, 3, 4, 5 }, 2, 9)]
+    [InlineData(new[] { 1, 4, 4 }, 3, 4)]
+    public void Test(int[] nums, int k, int expected)
+    {
+        var res = SplitArray(nums, k);
+        Assert.Equal(expected, res);
+    }
+
+    public int SplitArray(int[] nums, int k)
+    {
+        int left = 0;
+        int right = 0;
+
+        foreach (int num in nums)
+        {
+            left = Math.Max(left, num); // Максимальный элемент
+            right += num; // Сумма всех элементов
+        }
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (CanSplit(nums, k, mid))
+            {
+                right = mid; // Пробуем уменьшить максимальную сумму
+            }
+            else
+            {
+                left = mid + 1; // Текущий предел слишком мал
+            }
+        }
+
+        return left;
+    }
+
+    private bool CanSplit(int[] nums, int k, int maxSum)
+    {
+        int currentSum = 0;
+        int subarraysCount = 1;
+
+        foreach (int num in nums)
+        {
+            if (currentSum + num > maxSum)
+            {
+                subarraysCount++;
+                currentSum = num;
+                if (subarraysCount > k)
+                    return false;
+            }
+            else
+            {
+                currentSum += num;
+            }
+        }
+
+        return true;
+    }
+
+    // работает, но медленно
+    // Идея, Мы не знаем точно, как делить массив, но мы знаем диапазон, в котором лежит результат:
+    // left - Самое большое число в массиве, right - Сумма всех элементов массива
+    public int MySplitArray(int[] nums, int k)
+    {
+        var left = nums.Max();
+        var right = nums.Sum();
+        var sum = 0;
+
+        while (left < right)
+        {
+            var mid = left + (right - left) / 2;
+            var i = 0;
+            var times = 0;
+            while (i < nums.Length)
+            {
+                sum = nums[i];
+                while (i < nums.Length - 1 && sum + nums[i + 1] <= mid)
+                {
+                    sum += nums[i + 1];
+                    i++;
+                }
+                times++;
+                i++;
+            }
+
+            if (times <= k)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        var j = 0;
+        var res = new List<int>(k);
+        while (j < nums.Length)
+        {
+            sum = nums[j];
+            while (j < nums.Length - 1 && sum + nums[j + 1] <= left)
+            {
+                sum += nums[j + 1];
+                j++;
+            }
+            res.Add(sum);
+            j++;
+        }
+
+        return res.Max();
+    }
+}
