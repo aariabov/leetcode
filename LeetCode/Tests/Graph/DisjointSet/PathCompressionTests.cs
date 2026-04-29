7da559947b79c8ea701b92c9f989a02afb196386
aariabov <aariabov@mail.ru>
2026-04-29 13:46:13 +0500

    Path Compression

---
+++ LeetCode/Tests/Graph/DisjointSet/PathCompressionTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests.Graph.DisjointSet;
+
+public class PathCompressionTests
+{
+    [Fact]
+    public void Test()
+    {
+        var uf = new UnionFind(10);
+
+        // 1-2-5-6-7 3-8-9 4
+        uf.Union(1, 2);
+        uf.Union(2, 5);
+        uf.Union(5, 6);
+        uf.Union(6, 7);
+        uf.Union(3, 8);
+        uf.Union(8, 9);
+
+        Assert.True(uf.Connected(1, 5));
+        Assert.True(uf.Connected(5, 7));
+        Assert.False(uf.Connected(4, 9));
+
+        // 1-2-5-6-7 3-8-9-4
+        uf.Union(9, 4);
+        Assert.True(uf.Connected(4, 9));
+    }
+
+    private class UnionFind
+    {
+        private int[] root;
+
+        public UnionFind(int size)
+        {
+            root = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                root[i] = i;
+            }
+        }
+
+        // Path Compression: updates root[x] to the actual root during recursion
+        public int Find(int x)
+        {
+            if (x == root[x])
+            {
+                return x;
+            }
+            return root[x] = Find(root[x]);
+        }
+
+        public void Union(int x, int y)
+        {
+            int rootX = Find(x);
+            int rootY = Find(y);
+            if (rootX != rootY)
+            {
+                root[rootY] = rootX;
+            }
+        }
+
+        public bool Connected(int x, int y)
+        {
+            return Find(x) == Find(y);
+        }
+    }
+}
