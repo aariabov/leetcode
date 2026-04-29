dde0b473dc3b532670ad280ea86a5ae680d6ab49
aariabov <aariabov@mail.ru>
2026-04-29 14:57:39 +0500

    PathCompressionAndUnionByRankTests

---
+++ LeetCode/Tests/Graph/DisjointSet/PathCompressionAndUnionByRankTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Tests.Graph.DisjointSet;
+
+public class PathCompressionAndUnionByRankTests
+{
+    [Fact]
+    public void Test()
+    {
+        var uf = new UnionFind(10);
+
+        // 1-2-5-6-7 3-8-9 4
+        uf.Union(1, 2);
+        uf.Union(2, 5);
+        uf.Union(5, 6);
+        uf.Union(6, 7);
+        uf.Union(3, 8);
+        uf.Union(8, 9);
+
+        Assert.True(uf.Connected(1, 5));
+        Assert.True(uf.Connected(5, 7));
+        Assert.False(uf.Connected(4, 9));
+
+        // 1-2-5-6-7 3-8-9-4
+        uf.Union(9, 4);
+        Assert.True(uf.Connected(4, 9));
+    }
+
+    private class UnionFind
+    {
+        private int[] root;
+        private int[] rank;
+
+        public UnionFind(int size)
+        {
+            root = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        // Find with Path Compression
+        public int Find(int x)
+        {
+            if (x == root[x])
+            {
+                return x;
+            }
+            // Recursively finds the root and flattens the structure
+            return root[x] = Find(root[x]);
+        }
+
+        // Union by Rank
+        public void Union(int x, int y)
+        {
+            int rootX = Find(x);
+            int rootY = Find(y);
+
+            if (rootX != rootY)
+            {
+                if (rank[rootX] > rank[rootY])
+                {
+                    root[rootY] = rootX;
+                }
+                else if (rank[rootX] < rank[rootY])
+                {
+                    root[rootX] = rootY;
+                }
+                else
+                {
+                    // If ranks are equal, pick one as root and increase its rank
+                    root[rootY] = rootX;
+                    rank[rootX] += 1;
+                }
+            }
+        }
+
+        public bool Connected(int x, int y)
+        {
+            return Find(x) == Find(y);
+        }
+    }
+}
