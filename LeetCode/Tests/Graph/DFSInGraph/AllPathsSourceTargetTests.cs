2219ad82147fad28464ca71f4f9948731916d256
aariabov <aariabov@mail.ru>
2026-05-06 10:41:12 +0500

    [Все пути между вершинами](https://leetcode.com/explore/learn/card/graph/620/breadth-first-search-in-graph/3853/)

--- LeetCode/Tests/Graph/DFSInGraph/AllPathsSourceTargetTests.cs
+++ LeetCode/Tests/Graph/DFSInGraph/AllPathsSourceTargetTests.cs
@@ -1,6 +1,9 @@
-﻿namespace Tests.Graph.DFSInGraph;
+﻿using FluentAssertions;
+
+namespace Tests.Graph.DFSInGraph;
 
 // [Все пути между вершинами](https://leetcode.com/explore/learn/card/graph/619/depth-first-search-in-graph/3849/)
+// https://leetcode.com/explore/learn/card/graph/620/breadth-first-search-in-graph/3853/
 public class AllPathsSourceTargetTests
 {
     public static IEnumerable<object[]> MatrixData =>
@@ -23,7 +26,7 @@ public class AllPathsSourceTargetTests
     public void Test(int[][] nums, int[][] expected)
     {
         var result = AllPathsSourceTarget(nums);
-        Assert.Equal(expected, result);
+        result.Should().BeEquivalentTo(expected);
     }
 
     public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
@@ -56,32 +59,58 @@ public class AllPathsSourceTargetTests
         return result;
     }
 
-    // самый быстрый вариант
-    public IList<IList<int>> AllPathsSourceTargetBack(int[][] graph)
+    public IList<IList<int>> AllPathsSourceTargetBFS(int[][] graph)
     {
         var result = new List<IList<int>>();
-        var path = new List<int> { 0 }; // Начинаем с нулевого узла
+        var queue = new Queue<List<int>>();
+        queue.Enqueue(new List<int> { 0 });
+        while (queue.Count > 0)
+        {
+            var path = queue.Dequeue();
+            var last = path[^1];
+            if (last == graph.Length - 1)
+            {
+                result.Add(path);
+            }
 
-        Dfs(0, graph, path, result);
+            foreach (var neighbor in graph[last])
+            {
+                var newList = new List<int>(path);
+                newList.Add(neighbor);
+                queue.Enqueue(newList);
+            }
+        }
 
         return result;
     }
 
-    private void Dfs(int node, int[][] graph, List<int> path, List<IList<int>> result)
+    // самый быстрый вариант
+    // Идея: рекурсивно идем в глубину, собираем path, при возврате из рекурсии удаляем последний узел (Бэктрекинг), таким образом path всегда актуальный
+    public IList<IList<int>> AllPathsSourceTargetBack(int[][] graph)
     {
-        // Если дошли до последнего узла (n - 1)
-        if (node == graph.Length - 1)
-        {
-            result.Add(new List<int>(path));
-            return;
-        }
+        var result = new List<IList<int>>();
+        var path = new List<int> { 0 };
 
-        // Проходим по всем соседям текущего узла
-        foreach (int neighbor in graph[node])
+        Dfs();
+        return result;
+
+        void Dfs()
         {
-            path.Add(neighbor); // Добавляем соседа в путь
-            Dfs(neighbor, graph, path, result); // Рекурсивный переход
-            path.RemoveAt(path.Count - 1); // Бэктрекинг: удаляем узел перед возвратом
+            int node = path[^1];
+            // Если дошли до последнего узла (n - 1)
+            if (node == graph.Length - 1)
+            {
+                result.Add(new List<int>(path)); // добавляем копию списка в результат
+                return;
+            }
+
+            // Проходим по всем соседям текущего узла
+            foreach (int neighbor in graph[node])
+            {
+                path.Add(neighbor);
+                Dfs();
+                path.RemoveAt(path.Count - 1); // Бэктрекинг: удаляем узел перед возвратом
+            }
         }
     }
 
