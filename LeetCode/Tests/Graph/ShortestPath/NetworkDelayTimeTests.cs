9d664b86269adf599d2959302159fef0c78e6991
aariabov <aariabov@mail.ru>
2026-05-07 18:40:51 +0500

    dsf

--- LeetCode/Tests/Graph/ShortestPath/NetworkDelayTimeTests.cs
+++ LeetCode/Tests/Graph/ShortestPath/NetworkDelayTimeTests.cs
@@ -9,6 +9,7 @@ public class NetworkDelayTimeTests
             new object[] { new int[][] { [2, 1, 1], [2, 3, 1], [3, 4, 1] }, 4, 2, 2 },
             new object[] { new int[][] { [1, 2, 1] }, 2, 1, 1 },
             new object[] { new int[][] { [1, 2, 1] }, 2, 2, -1 },
+            new object[] { new int[][] { [1, 2, 1], [1, 3, 2], [2, 4, 10], [3, 4, 3] }, 4, 1, 5 },
         };
 
     [Theory]
