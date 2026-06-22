21bafe75228835afd538e2d5fd245ebf6407d261
aariabov <aariabov@mail.ru>
2026-06-22 22:25:14 +0500

    [Найти единственный неповторяющийся элемент в массиве](https://leetcode.com/explore/learn/card/bit-manipulation/670/bit-manipulation-practice/4266/)

---
+++ LeetCode/Tests/BitManipulation/Practice/SingleNumberTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests.BitManipulation.Practice;
+
+public class SingleNumberTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 2, 1 }, 1)]
+    [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
+    [InlineData(new int[] { 1 }, 1)]
+    public void Test(int[] arr, int expected)
+    {
+        var result = SingleNumber(arr);
+        Assert.Equal(expected, result);
+    }
+
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
